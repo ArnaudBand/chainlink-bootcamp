d107c0802be4537ecf9400b01c44fd7815853800
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-10 10:19:27 +0000

    Add Register contract to store and retrieve information

---
+++ src/Register.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+// Address: 0x5a76C0F317099fF1D47AbC41fbF4a6288bACdB83
+
+contract Register {
+  string private info;
+
+  function setInfo(string memory _info) public {
+    info = _info;
+  }
+
+  function getInfo() public view returns (string memory) {
+    return info;
+  }
+}
